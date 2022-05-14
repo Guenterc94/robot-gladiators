5750c0cb2071b74425c013c4d001fefdb7695eab
Guenter <castrilloguenter@gmail.com>
2022-05-14 14:06:40 -0700

    added to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,7 +20,9 @@ var fight = function() {
     if (promptFight === "fight" || promptFight === "FIGHT") {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
-        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            );
   
         // check enemy's health
         if (enemyHealth <= 0) {
@@ -31,7 +33,9 @@ var fight = function() {
   
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
-        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            );
   
         // check player's health
         if (playerHealth <= 0) {
@@ -41,22 +45,25 @@ var fight = function() {
         }
 
     // if player choses to skip
-    } 
-    else if (promptFight === "skip" || promptFight === "SKIP") {
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-        // if yes (true), leave fight
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // if yes (true), leave fight
+    if (confirmSkip) {
+         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 2;
-        }
-        // if no (false), ask question again by running fight() again
-        else {
-            fight();
-        }
+        playerMoney = playerMoney - 2;
     }
-}
+    // if no (false), ask question again by running fight() again
+    else {
+        fight();
+    }
+    } else {
+    window.alert("You need to pick a valid option. Try again!");
+    }
+};  // end of fight function
+
+// run fight function to start game
 fight();
 
